37e2d5c13449dd56d222e24a94c65ad3fc7e95a4
Ben Hyrman <ben.hyrman@gmail.com>
2020-05-09 10:43:13 -0400

    Merge data access together for now
    I think this is a good basis for now but in the future maybe
    introduce a domain interface that has an id
    then abstract the load and upsert back to a different class

--- pkg/models/snippet.go
+++ pkg/models/snippet.go
@@ -2,7 +2,10 @@ package models
 
 import (
 	"encoding/binary"
+	"encoding/json"
 	"time"
+
+	bolt "go.etcd.io/bbolt"
 )
 
 type Snippet struct {
@@ -21,13 +24,64 @@ type Snippets []*Snippet
 func GetSnippet(s *Store, id uint64) (Snippet, error) {
 	snippet := Snippet{}
 
-	err := s.get(bucketName, keyFromID(id), &snippet)
-
-	if err != nil {
-		err = ErrNoResult
-	}
+	err := loadByID(s, id, &snippet)
 
 	return snippet, err
+}
+
+func upsert(s *Store, snippet *Snippet) error {
+	err := s.DB.Update(func(tx *bolt.Tx) error {
+		// Create the bucket
+		bucket, e := tx.CreateBucketIfNotExists([]byte(bucketName))
+		if e != nil {
+			return e
+		}
+
+		if snippet.ID == 0 {
+			id, _ := bucket.NextSequence()
+			snippet.ID = id
+		}
+
+		// Encode the record
+		encoded, e := json.Marshal(snippet)
+		if e != nil {
+			return e
+		}
+
+		// Store the record
+		e = bucket.Put(keyFromID(snippet.ID), encoded)
+		if e != nil {
+			return e
+		}
+		return nil
+	})
+	return err
+}
+
+func loadByID(s *Store, id uint64, snippet *Snippet) error {
+	err := s.DB.View(func(tx *bolt.Tx) error {
+		// Get the bucket
+		bucket := tx.Bucket([]byte(bucketName))
+		if bucket == nil {
+			return ErrNoResult
+		}
+
+		// Retrieve the record
+		encoded := bucket.Get(keyFromID(id))
+		if encoded == nil {
+			return ErrNoResult
+		}
+
+		// Decode the record
+		e := json.Unmarshal(encoded, &snippet)
+		if e != nil {
+			return e
+		}
+
+		return nil
+	})
+
+	return err
 }
 
 func keyFromID(v uint64) []byte {
--- pkg/models/store.go
+++ pkg/models/store.go
@@ -1,61 +1,9 @@
 package models
 
 import (
-	"encoding/json"
-
 	bolt "go.etcd.io/bbolt"
 )
 
 type Store struct {
 	DB *bolt.DB
-}
-
-func (s *Store) update(bucket string, key []byte, dataStruct interface{}) error {
-	err := s.DB.Update(func(tx *bolt.Tx) error {
-		// Create the bucket
-		bucket, e := tx.CreateBucketIfNotExists([]byte(bucket))
-		if e != nil {
-			return e
-		}
-
-		// Encode the record
-		encoded, e := json.Marshal(dataStruct)
-		if e != nil {
-			return e
-		}
-
-		// Store the record
-		e = bucket.Put(key, encoded)
-		if e != nil {
-			return e
-		}
-		return nil
-	})
-	return err
-}
-
-func (s *Store) get(bucket string, key []byte, dataStruct interface{}) error {
-	err := s.DB.View(func(tx *bolt.Tx) error {
-		// Get the bucket
-		bucket := tx.Bucket([]byte(bucket))
-		if bucket == nil {
-			return bolt.ErrBucketNotFound
-		}
-
-		// Retrieve the record
-		encoded := bucket.Get(key)
-		if len(encoded) < 1 {
-			return bolt.ErrInvalid
-		}
-
-		// Decode the record
-		e := json.Unmarshal(encoded, &dataStruct)
-		if e != nil {
-			return e
-		}
-
-		return nil
-	})
-
-	return err
 }
